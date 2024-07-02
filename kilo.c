820aa35c21c23137c118faa51bcd3091e5668c55
strnge <max.wi.strange@gmail.com>
2024-07-01 21:55:33 -0400

    added cursor movement

--- kilo.c
+++ kilo.c
@@ -189,7 +189,10 @@ void editorRefreshScreen(){
     
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6);
     
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -198,15 +201,38 @@ void editorRefreshScreen(){
 
 /*input*/
 
+void editorMoveCursor(char key) {
+  switch (key) {
+        case 'a':
+            E.cx--;
+            break;  
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;          
+        case 's':
+            E.cy++;
+            break;
+        }
+}
+
 void editorProcessKeypress() {
     char c = editorReadKey();
 
     /*switch case for check the different types of ctrl keys*/
     switch (c) {
         case CTRL_KEY('q'):
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
-        exit(0);
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+        case 'w':
+        case 's':
+        case 'a':
+        case 'd':
+        editorMoveCursor(c);
         break;
     }
 }
